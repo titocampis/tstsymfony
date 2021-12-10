c785ce135ead82d771f0fd7cfae34de82f501747
titocampis <andreasscorelli@gmail.com>
2021-12-10 09:13:42 +0100

    Primera versión

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+     * @Route("/random/number",  name="app_lucky_number")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
